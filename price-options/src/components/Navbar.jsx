3aad4f8123577a025cf47c4e37e29a2dcf867f74
Bithe <farzana.hossain5@gmail.com>
2024-03-21 02:21:41 +0600

    pricdd

---
+++ price-options/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+
+const Navbar = () => {
+    const routes = [
+        { id: 1, path: "/", name: "Home" },
+        { id: 2, path: "/about", name: "About" },
+        { id: 3, path: "/services", name: "Services" },
+        { id: 4, path: "/contact", name: "Contact" },
+        { id: 5, path: "/dashboard", name: "Dashboard" }
+      ];
+    return (
+        <div>
+            <h2>routes</h2>
+        </div>
+    );
+};
+
+export default Navbar;
